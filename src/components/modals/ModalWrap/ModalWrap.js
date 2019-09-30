510f733785df4e151c677070459dabc5715aebff
Kurkulya <kulinich_v@hotmail.com>
2019-10-01 01:55:36 +0300

    splitted modal implementation

---
+++ src/components/modals/ModalWrap/ModalWrap.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Modal } from 'semantic-ui-react';
+import PropTypes from 'prop-types';
+import { useSwitchState } from 'src/hooks/stateHooks';
+
+const ModalWrap = ({ children, ...restProps }) => {
+    const [open, openModal, closeModal] = useSwitchState(false);
+
+    return (
+        <Modal
+            open={open}
+            centered={false}
+            onOpen={openModal}
+            onClose={closeModal}
+            {...restProps}
+        >
+            {children({ closeModal })}
+        </Modal>
+    );
+};
+
+ModalWrap.propTypes = {
+    children: PropTypes.oneOfType([
+        PropTypes.arrayOf(PropTypes.node),
+        PropTypes.node,
+    ]).isRequired, 
+};
+
+export default ModalWrap;
